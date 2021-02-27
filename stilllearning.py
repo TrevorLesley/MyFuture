8e49c3fd3466abe745187fe95b919d7f8ded4940
MyFuture <newuser@News-MacBook-Air.local>
2021-02-27 12:00:39 -0500

    Refresher

---
+++ stilllearning.py
@@ -0,0 +1,332 @@
+.
+>>> print("Hello, World")
+Hello, World
+>>> help
+Type help() for interactive help, or help(object) for help about object.
+>>> help(shortcuts)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    help(shortcuts)
+NameError: name 'shortcuts' is not defined
+>>> help()
+
+Welcome to Python 3.9's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the Internet at https://docs.python.org/3.9/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> "modules"
+
+Please wait a moment while I gather a list of all available modules...
+
+
+Warning (from warnings module):
+  File "/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/setuptools/distutils_patch.py", line 25
+    warnings.warn(
+UserWarning: Distutils was imported before Setuptools. This usage is discouraged and may exhibit undesirable behaviors or errors. Please use Setuptools' objects directly or at least import Setuptools first.
+FirstProgram        _testinternalcapi   getopt              runpy
+__future__          _testmultiphase     getpass             sched
+_abc                _thread             gettext             secrets
+_aix_support        _threading_local    glob                select
+_ast                _tkinter            graphlib            selectors
+_asyncio            _tracemalloc        grp                 setuptools
+_bisect             _uuid               gzip                shelve
+_blake2             _warnings           hashlib             shlex
+_bootlocale         _weakref            heapq               shutil
+_bootsubprocess     _weakrefset         hello_world         signal
+_bz2                _xxsubinterpreters  hmac                site
+_codecs             _xxtestfuzz         html                smtpd
+_codecs_cn          _zoneinfo           http                smtplib
+_codecs_hk          abc                 idlelib             sndhdr
+_codecs_iso2022     aifc                imaplib             socket
+_codecs_jp          antigravity         imghdr              socketserver
+_codecs_kr          argparse            imp                 sqlite3
+_codecs_tw          array               importlib           sre_compile
+_collections        ast                 inspect             sre_constants
+_collections_abc    asynchat            io                  sre_parse
+_compat_pickle      asyncio             ipaddress           ssl
+_compression        asyncore            itertools           stat
+_contextvars        atexit              json                statistics
+_crypt              audioop             keyword             string
+_csv                base64              lib2to3             stringprep
+_ctypes             bdb                 linecache           struct
+_ctypes_test        binascii            locale              subprocess
+_curses             binhex              logging             sunau
+_curses_panel       bisect              lzma                symbol
+_datetime           builtins            mailbox             symtable
+_dbm                bz2                 mailcap             sys
+_decimal            cProfile            marshal             sysconfig
+_elementtree        calendar            math                syslog
+_functools          cgi                 mimetypes           tabnanny
+_hashlib            cgitb               mmap                tarfile
+_heapq              chunk               modulefinder        telnetlib
+_imp                cmath               multiprocessing     tempfile
+_io                 cmd                 netrc               termios
+_json               code                nis                 test
+_locale             codecs              nntplib             textwrap
+_lsprof             codeop              ntpath              this
+_lzma               collections         nturl2path          threading
+_markupbase         colorsys            numbers             time
+_md5                compileall          opcode              timeit
+_multibytecodec     concurrent          operator            tkinter
+_multiprocessing    configparser        optparse            token
+_opcode             contextlib          os                  tokenize
+_operator           contextvars         parser              trace
+_osx_support        copy                pathlib             traceback
+_peg_parser         copyreg             pdb                 tracemalloc
+_pickle             crypt               pickle              tty
+_posixshmem         csv                 pickletools         turtle
+_posixsubprocess    ctypes              pip                 turtledemo
+_py_abc             curses              pipes               types
+_pydecimal          dataclasses         pkg_resources       typing
+_pyio               datetime            pkgutil             unicodedata
+_queue              dbm                 platform            unittest
+_random             decimal             plistlib            urllib
+_scproxy            difflib             poplib              uu
+_sha1               dis                 posix               uuid
+_sha256             distutils           posixpath           venv
+_sha3               doctest             pprint              warnings
+_sha512             easy_install        profile             wave
+_signal             email               pstats              weakref
+_sitebuiltins       encodings           pty                 webbrowser
+_socket             ensurepip           pwd                 wsgiref
+_sqlite3            enum                py_compile          xdrlib
+_sre                errno               pyclbr              xml
+_ssl                faulthandler        pydoc               xmlrpc
+_stat               fcntl               pydoc_data          xxlimited
+_statistics         filecmp             pyexpat             xxsubtype
+_string             fileinput           queue               zipapp
+_strptime           fnmatch             quopri              zipfile
+_struct             formatter           random              zipimport
+_symtable           fractions           re                  zlib
+_sysconfigdata__darwin_darwin ftplib              readline            zoneinfo
+_testbuffer         functools           reprlib             
+_testcapi           gc                  resource            
+_testimportmultiple genericpath         rlcompleter         
+
+Enter any module name to get more help.  Or, type "modules spam" to search
+for modules whose name or summary contain the string "spam".
+
+help> 
+
+You are now leaving help and returning to the Python interpreter.
+If you want to ask for help on a particular object directly from the
+interpreter, you can type "help(object)".  Executing "help('string')"
+has the same effect as typing a particular string at the help> prompt.
+>>> runpy
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    runpy
+NameError: name 'runpy' is not defined
+>>> 
+=============== RESTART: /Users/newuser/Documents/hello_world.py ===============
+Hello, World
+>>> # http://tinyurl.com/h79ob7s
+>>> 
+================ RESTART: /Users/newuser/Documents/repitition.py ===============
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+Hello, World
+>>> 
+
+# http://tinyurl.com/hut6nwu
+
+# This is a comment
+print("This is for learning comments.")
+
+============= RESTART: /Users/newuser/Documents/learningcomments.py ============
+This is for learning comments.
+>>> 
+>>> 2+2
+4
+>>> 2-2
+0
+>>> 4/2
+2.0
+>>> 2*2
+4
+>>> 
+
+y = 100
+print(y)
+
+============ RESTART: /Users/newuser/Documents/learningvariables.py ============
+100
+>>> 
+
+y = 100
+print(y)
+
+
+y = 200
+print(y)
+
+======== RESTART: /Users/newuser/Documents/changingvaluesofvariables.py ========
+100
+200
+>>> 
+
+x = 10
+y = 10
+z = x + y
+print(z)
+a = x - y
+print(a)
+
+============== RESTART: /Users/newuser/Documents/mathematicsmk1.py =============
+20
+0
+>>> x = 10
+x = x - 1
+print(x)
+
+=============== RESTART: /Users/newuser/Documents/decrementing.py ==============
+9
+>>> 
+
+x = 10
+x = x + 1
+print(x)
+
+=============== RESTART: /Users/newuser/Documents/incrementing.py ==============
+11
+>>> 
+
+x = 10
+x += 1
+print(x)
+
+
+y = 10
+y -= 1
+print(y)
+
+============ RESTART: /Users/newuser/Documents/shorterupsanddowns.py ===========
+11
+9
+>>> 
+
+hi = "Howdy!"
+print(hi)
+
+=========== RESTART: /Users/newuser/Documents/confusingbutitworks.py ===========
+Howdy!
+>>> 
+
+a_floater = 10.80
+print(a_floater)
+
+
+we_boolin = True
+print(we_boolin)
+
+======== RESTART: /Users/newuser/Documents/moreconfusionlessconfusing.py =======
+10.8
+True
+>>> 
