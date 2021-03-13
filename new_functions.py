01dd60b5c746623d1373b3b0167f10b281b55bac
Trevor Lesley <tlesley96@gmail.com>
2021-03-13 11:04:40 -0500

    no_more_shell

---
+++ new_functions.py
@@ -0,0 +1,22 @@
+def f(x):
+    return (x * 2)
+    
+result = f(2)
+print(result)
+
+
+################# 
+
+def z(v):
+    return (v+1)
+
+y = z(4)
+print(y)
+
+if y == 5:
+    print("y is 5")
+else:
+    print("y is not 5")
+
+
+#################
