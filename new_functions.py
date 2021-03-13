4e08262d576941b7fb597fc11ee0f3fe3d8520b6
Trevor Lesley <tlesley96@gmail.com>
2021-03-13 15:56:02 -0500

    doodlebrain

--- new_functions.py
+++ new_functions.py
@@ -1,50 +1,71 @@
-def f(x):
+" Defining Functions"
+
+def a(x):
     return (x * 2)
     
-result = f(2)
-print(result, " result problem 1")
-
-
 ################# 
-
 #These return statements need parentheses
 
 def z(v):
     return (v+1)
 
-y = z(4)
-print(y)
-
-if y == 5:
-    print("y is 5 result problem 2")
-else:
-    print("y is not 5 result problem 2")
-
-
 #################
-
-def f():
+def h():
     return (1+1)
 
 
-result = f()
-print(result)
-
 #################
-
-def f(x, y, z):
+def g(x, y, z):
     return (x+y+z)
 
 
-result = f(1, 2, 3)
-print(result, " result problem 3")
-
 ##########################
 
-age = input("Enter your age:")
-int_age = int(age)
-if int_age < 21:
-    print("Youngin")
-else:
-    print("Ancient")
+# age = input("Enter your age:")
+# int_age = int(age)
+# if int_age < 21:
+#     print("Youngin")
+# else:
+#     print("Ancient")
  
+################
+
+#Optional parameters
+
+def f(x=2):
+    return (x**x)
+
+###############
+# division 
+a = input("Type a number:")
+b = input("Type another:")
+a = int(a)
+b = int(b)
+
+
+""" Function Calls """
+# result = a(2)
+# print(result, " result problem 1")
+
+
+# result = g(1, 2, 3)
+# print(result, " result problem 3")
+
+
+# result = h()
+# print(result, " Result H")
+
+
+# y = z(4)
+# print(y, " Result Y")
+# if y == 5:
+#     print("y is 5 result problem 2")
+# else:
+#     print("y is not 5 result problem 2")
+
+
+#print(f())
+#print(f(4))
+
+#division
+print(a / b)
